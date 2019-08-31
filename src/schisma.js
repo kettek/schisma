2bef8c2d3749d9d949ec6aa299a7a58c6dfd1579
kts of kettek (Tyger) <kettek1@kettek.net>
2019-08-31 05:48:14 -0700

    Implement object to schema conforming

--- src/schisma.js
+++ src/schisma.js
@@ -1,36 +1,4 @@
 /**
- * SchismaError is the type used to create errors during schema validation.
- */
-class SchismaError {
-  constructor(code=0, extra) {
-    this.code     = code
-    if (extra.value !== undefined) this.value = extra.value
-    if (extra.where !== undefined) this.where = extra.where
-    if (extra.message !== undefined)  this.message  = extra.message
-    if (extra.expected !== undefined) this.expected = extra.expected
-    if (extra.received !== undefined) this.received = extra.received
-  }
-  static get UNHANDLED() {
-    return 0
-  }
-  static get BAD_TYPE() {
-    return 1
-  }
-  static get UNEXPECTED_KEY() {
-    return 2
-  }
-  static get MISSING_KEY() {
-    return 3
-  }
-  static get BAD_LENGTH() {
-    return 4
-  }
-  static get INVALID() {
-    return 5
-  }
-}
-
-/**
  * Schisma represents a schema used to validate or conform an object structure.
  * It can also be used to create new objects that use the schema's defaults.
  */
@@ -48,26 +16,31 @@ class Schisma {
   _understand(o) {
     if (Array.isArray(o)) {            // Array of some type.
       this.$type = o.map(v => new Schisma(v))
+      this.$default = this.$type.map(v => v.$default)
       // TODO: we need some stringified $type that converts deep arrays/objects into full k=>v pairs, kinda like JSON.stringify, but for types only.
     } else if (typeof o === 'object') {
       if (o.hasOwnProperty('$type') || o.hasOwnProperty('$validate')) { // Guaranteed SchismaConf
-        this.$type      = new Schisma(o.$type).$type
+        let sch         = new Schisma(o.$type)
+        this.$type      = sch.$type
         this.$validate  = o.$validate
         this.$required  = o.$required
-        this.$default   = o.$default || o.$type instanceof Function ? o.$type(0) : undefined
+        this.$default   = o.$default || sch.$default
       } else {                         // Traversable Obj.
         this.$type = {}
+        this.$default = {}
         for(let [k, v] of Object.entries(o)) {
           this.$type[k] = new Schisma(v)
+          this.$default[k] = this.$type[k].$default
         }
       }
     } else {                           // Primitives.
+      this.$ctor    = o
       this.$default = o()
       this.$type    = typeof this.$default
     }
   }
   /**
-   * validate validates the provided object against the schema.
+   * validates the provided object against the schema.
    * @param {Object} o Object to validate.
    * @param {Object} [conf] Configuration for fine-tuning what is considered an error.
    * @param {Object} [conf.allowBadLength=false] Allows arrays to be shorter than the schema defines.
@@ -127,10 +100,56 @@ class Schisma {
     }
     return errors
   }
-  conform(o, conf={allowBadLength:false,allowUnexpected:false,allowMissing:false}) {
-    if (this.$type instanceof Object) {
+  /**
+   * conforms the provided object to match the schema. Mis-matched types are
+   * converted to their proper types when possible.
+   * @param {Object} o 
+   * @param {Object} [conf] Configuration for fine-tuning how conforming works.
+   * @param {Object} [conf.removeUnexpected=true] Removes unexpected object keys.
+   * @param {Object} [conf.insertMissing=true] Inserts missing object keys with default values.
+   */
+  conform(o, conf={removeUnexpected:true, insertMissing:true}) {
+    // Check arrays.
+    if (Array.isArray(o)) {
+      if (!Array.isArray(this.$type)) {
+        return this.$default
+      } else {
+        let arr = [...o.slice(0, o.length), ...this.$default.slice(o.length)]
+        // Check each element on o's Array against schema's type modulo schema's type length. This means we pattern match.
+        for (let i = 0; i < arr.length; i++) {
+          arr[i] = this.$type[i%this.$type.length].conform(arr[i], conf)
+        }
+        return arr
+      }
+    // Check objects.
+    } else if (typeof o === 'object') {
+      if (!(this.$type instanceof Object)) {
+        return this.$default
+      } else {
+        // Generate a list of common keys and check if they are unexpected, missing, or fail validation.
+        let c = {}
+        for (let k of new Set([...Object.keys(this.$type), ...Object.keys(o)])) {
+          if (!this.$type.hasOwnProperty(k)) {
+            if (!conf.removeUnexpected) {
+              c[k] = o[k]
+            }
+          } else if (!o.hasOwnProperty(k)) {
+            if (conf.insertMissing) {
+              c[k] = this.$type[k].$default
+            }
+          } else {
+            c[k] = this.$type[k].conform(o[k], conf)
+          }
+        }
+        return c
+      }
+    // Check primitives.
+    } else {
+      if (typeof o !== this.$type) {
+        return this.$ctor(o)
+      }
+      return o
     }
-    // TODO: Generate a version of passed object that conforms to the schema, optionally removing unexpected keys.
   }
   /**
    * create creates a new object that conforms to schema using computed or
@@ -159,54 +178,34 @@ class Schisma {
   }
 }
 
-let schA = new Schisma({
-  a: Number,
-  b: Symbol,
-  c: [Boolean],
-  d: {
-    dA: String,
-  },
-  f: [String, Number],
-  g: {
-    $type: Number,
-  },
-  h: String,
-  i: {
-    $type: Boolean,
-  },
-  j: {
-    jA: {
-      jAa: Number,
-      jAb: {
-        $type: String,
-        $default: "Hey"
-      },
-      jAc: {
-        $type: {
-          jAcA: Number
-        }
-      }
-    }
+/**
+ * SchismaError is the type used to create errors during schema validation.
+ */
+class SchismaError {
+  constructor(code=0, extra) {
+    this.code     = code
+    if (extra.value !== undefined) this.value = extra.value
+    if (extra.where !== undefined) this.where = extra.where
+    if (extra.message !== undefined)  this.message  = extra.message
+    if (extra.expected !== undefined) this.expected = extra.expected
+    if (extra.received !== undefined) this.received = extra.received
   }
-})
-
-let objA = {
-  a: 32,
-  b: Symbol(100),
-  c: [true, BigInt(123)],
-  d: {
-    dA: "hi",
-    dB: 32,
-  },
-  f: ["string"],
-  g: 30,
+  static get UNHANDLED() {
+    return 0
+  }
+  static get BAD_TYPE() {
+    return 1
+  }
+  static get UNEXPECTED_KEY() {
+    return 2
+  }
+  static get MISSING_KEY() {
+    return 3
+  }
+  static get BAD_LENGTH() {
+    return 4
+  }
+  static get INVALID() {
+    return 5
+  }
 }
-
-let errors = schA.validate(objA, { allowBadLength: false, allowUnexpected: false, allowMissing: false })
-console.log(errors)
-
-let parsed = schA.conform(objA)
-
-let defObj = schA.create()
-console.log(defObj)
-console.log(schA.validate(defObj))

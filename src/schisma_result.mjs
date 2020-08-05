234b68b25ad47fff566d05e609e95b5cd958487a
kts of kettek (nyaa) <kettek1@kettek.net>
2020-08-05 16:10:28 -0700

    Change to MPL, bump to version 0.7.6

--- src/schisma_result.mjs
+++ src/schisma_result.mjs
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2020 Ketchetwahmeegwun T. Southall
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /**
  * SchismaResult is the type used to create errors and results during schema validation.
  */
